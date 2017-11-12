1733b40dd2bfee11dd44ddcca45791454038c357
Eric Carlisle <eric@ericcarlisle.com>
2017-11-12 18:40:29 -0500

    Add SQL build scripts.

---
+++ src/sql/insert-test-data.sql
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+	<title>Widgets Unlimited Ltd.</title>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<link type="text/css" rel="stylesheet" href="/css/main.css">
+	<script type="/js/main.js"></script>
+</head>
+<body>
+
+
+</body>
+</html>
