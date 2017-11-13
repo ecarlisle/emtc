3eeb18e8b6021f35b994fc39c4b2266f3cb51653
Eric Carlisle <eric@ericcarlisle.com>
2017-11-13 11:21:13 -0500

    Start adding endpoints for inventory list and updating inventory count

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const sql = require('mssql')
+const http = require('http')
+const bodyParser = require('body-parser')
 
 // Configure Express server.
 const express = require('express'),
@@ -6,35 +8,88 @@ const express = require('express'),
 	port = 3000
 
 // Set routes for static files.
-app.use('/', express.static('pages'))
+app.use('/', express.static('static'))
+
+// Create an application/json parser
+var jsonParser = bodyParser.json()
 
 // Configure SQL Server connection.
 const dbConfig = {
-	user: '***',
-	password: '***',
-	server: '***',
-	database: '***',
+	user: 'eric',
+	password: 'TK421!!!',
+	server: 'eric.c59z71omoilm.us-east-1.rds.amazonaws.com',
+	database: 'emtc',
 	port: 1433,
 	encrypt: true,
 }
 
+// Create database pool, connect to database, and then start application.
 let pool;
-
-// Create database pool, then start application.
 const startApp = async () => {
-	pool = await sql.connect(dbConfig)
-	app.listen(3000)
+	try {
+		pool = await sql.connect(dbConfig)
+		app.listen(3000)
+	} catch(err) {
+
+		// NOTE: Actual error reporting would need to be a lot stronger.
+		// I've left try/catch out of query attempts to not repeat the
+		// not-so-useful console output.
+		console.log(err);
+	}
 }
 
-// Object containing all queries as prepared statements.
-const queries = {
-	getCategories: 'select * from dbo.widget_categories',
+// Requests a query and returns the first recordset in the returned object.
+const doQuery = async (qry) => {
+	const results = await pool.request().query(qry);
+	return results.recordset;
 }
 
-const doQuery = async (qry) => await pool.request().query(qry)
+// Return full widget inventory.
+app.get('/api/v1/widgets', function(req, res) {
+	(async () => {
+		res.setHeader('Content-Type', 'application/json')
+		try {
+			const results = await doQuery(`SELECT i.InventoryId, c.CategoryName, s.SizeName, f.FinishName, f.FinishHex , i.Quantity
+																		 FROM dbo.widget_inventory i
+																		 JOIN widget_sizes s ON s.SizeId = i.SizeId
+																		 JOIN widget_categories c ON c.CategoryId = i.CategoryId
+																		 JOIN widget_finishes f ON  f.FinishId = i.SizeId`)
+			res.send(results);
+		} catch(err) {
+			res.sendStatus(400)
+		}
+	})();
+})
 
-app.get ('/', function (req, res) {
-	res.send('Hello.')
+// Update the quantity of a category item.
+app.put('/api/v1/widgets/item', jsonParser, function(req, res) {
+	(async () => {
+		res.setHeader('Content-Type', 'application/json')
+		const paramQuantity = req.params.quantity
+		const paramId = req.params.id
+		console.log(req);
+		try {
+			const ps = new sql.PreparedStatement(pool)
+			ps.input('quantity', sql.Int)
+			ps.input('id', sql.Int)
+			ps.prepare(`UPDATE dbo.widget_inventory
+									SET Quantity = @quantity
+									WHERE InventoryId = @id`, err => {
+			// TODO: Error checks
+
+					ps.execute({quantity: paramQuantity, id: paramId}, (err, result) => {
+					// TODO: Error checks
+
+						ps.unprepare(err => {
+						// TODO: Error checks
+					})
+				})
+			})
+			res.send(true);
+		} catch(err) {
+			res.sendStatus(400)
+		}
+	})();
 })
 
 startApp()
