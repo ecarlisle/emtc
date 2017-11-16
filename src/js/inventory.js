bcc4937af10e920d2e2ef856aca1cba9562c862d
Eric Carlisle <eric@ericcarlisle.com>
2017-11-15 23:45:28 -0500

    Stringify JSON send to mssql Node module on endpoint.

--- src/js/inventory.js
+++ src/js/inventory.js
@@ -9,16 +9,25 @@ function updateQuantity(e) {
 	var inventoryId = $obj.data('id');
 	var quantity = $obj.siblings('input.quantity').val();
 
+	// TODO : Client-side validation on entered quantity.
+
 	$.ajax({
 		url: '/api/v1/widgets/item',
 		type: 'PUT',
-		data: {"quantity": quantity, "id": inventoryId},
+		data: JSON.stringify({"quantity": parseInt(quantity), "id": inventoryId}),
 		success: function(data) {
-			console.log(data);
-		}
+
+				// This console.log is just for debugging and would not be in
+				// production code. On an application the user should be
+				// provided some on-screen cue for waiting and result state.
+				if (data.rowsAffected.length === 1) {
+					console.log('Quantity update succeeded!');
+				} else {
+					console.log('Quantity update failed!');
+				}
+			}
 	});
 }
-
 
 function setEventHanders() {
 	$('.fa-save').on('click',function(e){
@@ -27,6 +36,11 @@ function setEventHanders() {
 }
 
 $(document).ready(function() {
+	$.ajaxSetup({
+		contentType : 'application/json',
+		processData : false,
+	});
+
 	$.ajax({
 		url: '/api/v1/widgets',
 		type: 'GET',
